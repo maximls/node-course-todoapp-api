93ef3679a6065e1938e56c00937b4e3bf1d1daff
Max Lysakovsky <maxim.lysakovsky@gmail.com>
2018-10-24 12:05:42 -0400

    added PATCH route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 
-var { mongoose } = require("./db/mongoose");
-var { Todo } = require("./models/todo");
-var { User } = require("./models/user");
-var { ObjectID } = require("mongodb");
-var port = process.env.PORT || 3000;
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
+const { ObjectID } = require("mongodb");
+const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -24,8 +25,9 @@ app.get("/todos", (req, res) => {
 });
 
 app.get("/todos/:id", (req, res) => {
-  if (!ObjectID.isValid(req.params.id)) {
-    return res.status(404).send();
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send("Invalid ID");
   }
 
   Todo.findById(req.params.id).then(
@@ -58,6 +60,38 @@ app.delete("/todos/:id", (req, res) => {
       res.status(400);
     }
   );
+});
+
+app.patch("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ["text", "completed"]);
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send("Invalid ID");
+  }
+  console.log(body);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(
+    id,
+    {
+      $set: body
+    },
+    {
+      new: true
+    }
+  )
+    .then(result => {
+      if (!result) {
+        return res.status(404).send();
+      }
+      res.status(200).send({ result });
+    })
+    .catch(err => res.status(400).send());
 });
 
 app.listen(port, () => {
