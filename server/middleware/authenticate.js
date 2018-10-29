80fde78890244150323aea41be7dd48ea38cdc56
Max Lysakovsky <maxim.lysakovsky@gmail.com>
2018-10-29 10:35:59 -0400

    added authentication to routes

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,20 @@
+const { User } = require("./../models/user");
+
+const authenticate = (req, res, next) => {
+  var token = req.header("x-auth");
+
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject();
+      }
+      console.log(user);
+      req.user = user;
+      req.token = token;
+
+      next();
+    })
+    .catch(e => res.status(401).send());
+};
+
+module.exports = { authenticate };
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
 const { ObjectID } = require("mongodb");
+const { authenticate } = require("./../server/middleware/authenticate");
 const port = process.env.PORT;
 
 const app = express();
@@ -110,6 +111,10 @@ app.post("/users", (req, res) => {
     .catch(e => {
       res.status(400).send(e);
     });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
